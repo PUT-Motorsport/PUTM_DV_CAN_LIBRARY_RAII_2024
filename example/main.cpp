2003e5817fcd46f7e6b37e294e5c429fb95ea3f1
KozAAAAA <mateusz-koza@outlook.com>
2023-06-27 12:41:46 +0200

    Add CanTx function to transmit data

--- example/main.cpp
+++ example/main.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
 #include <array>
 #include "../include/can_rx.hpp"
-// #include "../include/can_tx.hpp"
+#include "../include/can_tx.hpp"
 
 using namespace PUTM_CAN;
 
 int main()
 {
-    /* CONNECT TO THE CAN INTERFACE */
-    
-
-    CanRx<Apps_main> rx_apps_main("slcan0", NO_TIMEOUT);
-    CanRx<BMS_LV_main> rx_bms_lv_main("slcan0", NO_TIMEOUT);
-    CanRx<BMS_HV_main> rx_bms_hv_main("slcan0", NO_TIMEOUT);
-
-    CanRx<BMS_HV_main> rx_any("slcan0", NO_TIMEOUT);
-
-
-    while (1)
-    {
-        auto any = rx_any.receive_rtr();
-        std::cout << "any: " << any.voltage_sum << std::endl;
-    }
-
-    // while (1)
-    // {
-    //     rx_apps_main.receive();
-    //     rx_bms_lv_main.receive();
-    //     rx_bms_hv_main.receive();
-    // }
-
-    // CanTx can("slcan0");
-    // can.transmit(apps_main_lv);
-
-    // CanRx<can_frame> any_device("slcan0", 5); // connect right now, disconnect on destruction
-    // auto device = any_device.receive();
-
-    // CanRx<Apps_main_lv> apps_main_lv("slcan0", 3);
-    // auto apps_main = apps_main_lv.receive();
 
     /* RECEIVE DATA */
+    CanRx<BMS_HV_main> bms_hv_main("slcan0", NO_TIMEOUT);
+    auto bms_hv_main_data = bms_hv_main.receive();
+    std::cout << bms_hv_main_data.voltage_sum << std::endl;
 
     /* TRANSMIT DATA */
+
+    Apps_main apps_main;
+
+    apps_main.pedal_position = 1;
+    apps_main.counter = 2;
+    apps_main.position_diff = 3;
+    apps_main.device_state = Apps_states::Normal_operation;
+
+    CanTx tx("slcan0");
+    tx.transmit(apps_main);
 }
