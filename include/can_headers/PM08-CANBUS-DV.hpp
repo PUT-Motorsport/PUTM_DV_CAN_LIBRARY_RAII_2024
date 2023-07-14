caa6bb383b2e978c12de8a374b9bce2b61f34948
Adam Wasilewski <94369639+wasyl07@users.noreply.github.com>
2023-07-14 18:39:12 +0000

    Update PM08-CANBUS-DV.hpp

--- include/can_headers/PM08-CANBUS-DV.hpp
+++ include/can_headers/PM08-CANBUS-DV.hpp
@@ -17,15 +17,16 @@ struct __attribute__ ((packed)) DV_Ass{
 	AutonomousSystemStatus status;
 };
 
-
 const uint16_t DV_ASS_CAN_ID = 0x07;
 const uint8_t DV_ASS_CAN_DLC = sizeof(DV_Ass);
 const uint8_t DV_ASS_FREQUENCY = 1;
+
 
 enum struct DV_TC_control_status: uint8_t {
 	disabled,
 	current
 };
+
 
 struct __attribute__ ((packed)) DV_TC_control{
 	uint8_t cnt;
@@ -37,6 +38,14 @@ struct __attribute__ ((packed)) DV_TC_control{
 const uint16_t DV_TC_CAN_ID = 0x38;
 const uint8_t DV_TC_CAN_DLC = sizeof(DV_TC_control);
 const uint8_t DV_TC_FREQUENCY = 20;
+
+
+#ifndef PUTM_USE_CAN_FD
+
+const CAN_TxHeaderTypeDef can_tx_header_DV_ASS{
+DV_ASS_CAN_ID, 0xFFF, CAN_ID_STD, CAN_RTR_DATA, DV_ASS_CAN_DLC, DISABLE};
+
+#endif
 
 } // namespace can
 
