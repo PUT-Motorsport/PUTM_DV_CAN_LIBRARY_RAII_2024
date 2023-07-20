c66b82a0961b8271a353b88f4fd3a472bc403964
KozAAAAA <mateusz-koza@outlook.com>
2023-07-20 10:48:27 +0200

    Remove STM32 dependant code from the DV header

--- include/can_headers/PM08-CANBUS-DV.hpp
+++ include/can_headers/PM08-CANBUS-DV.hpp
@@ -39,14 +39,6 @@ const uint16_t DV_TC_CAN_ID = 0x38;
 const uint8_t DV_TC_CAN_DLC = sizeof(DV_TC_control);
 const uint8_t DV_TC_FREQUENCY = 20;
 
-
-#ifndef PUTM_USE_CAN_FD
-
-const CAN_TxHeaderTypeDef can_tx_header_DV_ASS{
-DV_ASS_CAN_ID, 0xFFF, CAN_ID_STD, CAN_RTR_DATA, DV_ASS_CAN_DLC, DISABLE};
-
-#endif
-
 } // namespace can
 
 #endif
