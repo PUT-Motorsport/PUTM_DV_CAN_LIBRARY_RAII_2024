ea66927870e64a42b6d5e1c8bcadd508f2646756
Adam <Adam_Wasilewski_99@proton.me>
2024-05-28 16:46:12 +0200

    Changed frontbox main to DriverInput

--- include/can_headers/PM09-CANBUS-FRONTBOX.hpp
+++ include/can_headers/PM09-CANBUS-FRONTBOX.hpp
@@ -17,18 +17,28 @@ enum struct Frontbox_states : uint8_t {
   Right_sensor_out_of_range_upper_bound,
 };
 
-struct __attribute__((packed)) Frontbox_main
-{
-  uint16_t pedal_position;  // pedal position form 0 – 1000 where 0 means pedal not pressed
-  uint8_t counter;          // counts from 0-255 to ensure all frames are recived
-  int8_t position_diff;     // pedal position derivative in [count / second]
-  Frontbox_states
-    device_state;  // -----------------------------------------------------------------
+struct __attribute__((packed)) DriverInput {
+  uint16_t pedalPosition;   // pedal position form 0 – 1000 where 0 means pedal not pressed
+  uint16_t brakePressureFront;
+  uint16_t brakePressureRear;
+  uint16_t steeringWheelPosition;
 };
 
-const uint16_t FRONTBOX_MAIN_CAN_ID = 0x5;
-const uint8_t FRONTBOX_MAIN_CAN_DLC = sizeof(Frontbox_main);
-const uint8_t FRONTBOX_MAIN_FREQUENCY = 100;
+struct __attribute__((packed)) FrontData {
+  uint8_t sc_state;
+  uint16_t frontLeftSuspension;
+  uint16_t frontRightSuspension;
+  uint8_t frontLeftHubTemperature;
+  uint8_t frontRightHubTemperature;
+};
+
+const uint16_t DRIVER_INPUT_CAN_ID = 0x5;
+const uint8_t DRIVER_INPUT_CAN_DLC = sizeof(DriverInput);
+const uint8_t DRIVER_INPUT_FREQUENCY = 100;
+
+const uint8_t FRONT_DATA_CAN_ID = 0x65;
+const uint8_t FRONT_DATA_CAN_DLC = sizeof(FrontData);
+const uint8_t FRONT_DATA_FREQUENCY = 10;
 
 }  // namespace PUTM_CAN
 #endif
