338aa5ddf8300cd20ca0e3aebeac29b528d9be23
KozAAAAA <mateusz-koza@outlook.com>
2024-07-13 19:41:12 +0200

    Update AmkActualValues

--- include/can_headers/PM09-CANBUS-AMK.hpp
+++ include/can_headers/PM09-CANBUS-AMK.hpp
@@ -7,72 +7,72 @@ namespace PUTM_CAN {
 // ---------- inverter -> can device ----------
 
 struct __attribute__((packed)) AmkStatus {
-  unsigned int AMK_bReserve1 : 8;
+  unsigned int reserve : 8;
 
-  unsigned int AMK_bSystemReady : 1;
-  unsigned int AMK_bError : 1;
-  unsigned int AMK_bWarn : 1;
-  unsigned int AMK_bQuitDcOn : 1;
-  unsigned int AMK_bDcOn : 1;
-  unsigned int AMK_bQuitInverterOn : 1;
-  unsigned int AMK_bInverterOn : 1;
-  unsigned int AMK_bDerating : 1;
+  bool system_ready : 1;
+  bool error : 1;
+  bool warn : 1;
+  bool quit_dc_on : 1;
+  bool dc_on : 1;
+  bool quit_inverter_on : 1;
+  bool inverter_on : 1;
+  bool derating : 1;
 };
 
 struct __attribute__((packed)) AmkFrontLeftActualValues1 {
-  AmkStatus AMK_Status;
-  int16_t AMK_ActualVelocity;
-  int16_t AMK_TorqueCurrent;
-  int16_t AMK_MagnetizingCurrent;
+  AmkStatus amk_status;
+  int16_t actual_velocity;
+  int16_t torque_current;
+  int16_t magnetizing_current;
 };
 
 struct __attribute__((packed)) AmkFrontLeftActualValues2 {
-  int16_t AMK_TempMotor;
-  int16_t AMK_TempInverter;
-  uint16_t AMK_ErrorInfo;
-  int16_t AMK_TempIGBT;
+  int16_t temp_motor;
+  int16_t temp_inverter;
+  uint16_t error_info;
+  int16_t temp_igbt;
 };
 
 struct __attribute__((packed)) AmkFrontRightActualValues1 {
-  AmkStatus AMK_Status;
-  int16_t AMK_ActualVelocity;
-  int16_t AMK_TorqueCurrent;
-  int16_t AMK_MagnetizingCurrent;
+  AmkStatus amk_status;
+  int16_t actual_velocity;
+  int16_t torque_current;
+  int16_t magnetizing_current;
 };
 
 struct __attribute__((packed)) AmkFrontRightActualValues2 {
-  int16_t AMK_TempMotor;
-  int16_t AMK_TempInverter;
-  uint16_t AMK_ErrorInfo;
-  int16_t AMK_TempIGBT;
+  int16_t temp_motor;
+  int16_t temp_inverter;
+  uint16_t error_info;
+  int16_t temp_igbt;
 };
 
 struct __attribute__((packed)) AmkRearLeftActualValues1 {
-  AmkStatus AMK_Status;
-  int16_t AMK_ActualVelocity;
-  int16_t AMK_TorqueCurrent;
-  int16_t AMK_MagnetizingCurrent;
+  AmkStatus amk_status;
+  int16_t actual_velocity;
+  int16_t torque_current;
+  int16_t magnetizing_current;
 };
 
 struct __attribute__((packed)) AmkRearLeftActualValues2 {
-  int16_t AMK_TempMotor;
-  int16_t AMK_TempInverter;
-  uint16_t AMK_ErrorInfo;
-  int16_t AMK_TempIGBT;
+  int16_t temp_motor;
+  int16_t temp_inverter;
+  uint16_t error_info;
+  int16_t temp_igbt;
 };
 
 struct __attribute__((packed)) AmkRearRightActualValues1 {
-  AmkStatus AMK_Status;
-  int16_t AMK_ActualVelocity;
-  int16_t AMK_TorqueCurrent;
-  int16_t AMK_MagnetizingCurrent;
+  AmkStatus amk_status;
+  int16_t actual_velocity;
+  int16_t torque_current;
+  int16_t magnetizing_current;
 };
 
 struct __attribute__((packed)) AmkRearRightActualValues2 {
-  int16_t AMK_TempMotor;
-  int16_t AMK_TempInverter;
-  uint16_t AMK_ErrorInfo;
-  int16_t AMK_TempIGBT;
+  int16_t temp_motor;
+  int16_t temp_inverter;
+  uint16_t error_info;
+  int16_t temp_igbt;
 };
 // --------------------------------------------
 
