3e398eb3fde9699e40dae51707b9135ac40a30d8
Adam <Adam_Wasilewski_99@proton.me>
2024-10-10 17:46:18 +0200

    Add xsens data, change pc main

--- include/can_headers/PM09-CANBUS-PC.hpp
+++ include/can_headers/PM09-CANBUS-PC.hpp
@@ -5,8 +5,16 @@
 namespace PUTM_CAN {
 
 struct __attribute__((packed)) PcMainData {
-  uint32_t time;
-  bool rtd : 1;
+    uint8_t rearRightInverterTemperature;
+    uint8_t rearLeftInverterTemperature;
+    uint8_t rearRightMotorTemperature;
+    uint8_t rearLeftMotorTemperature;
+    uint8_t vehicleSpeed : 6;
+    uint8_t power : 6;
+    uint16_t rpm : 15;
+	  bool invertersReady: 1;
+    bool rtd : 1;
+
 };
 
 }  // namespace PUTM_CAN
