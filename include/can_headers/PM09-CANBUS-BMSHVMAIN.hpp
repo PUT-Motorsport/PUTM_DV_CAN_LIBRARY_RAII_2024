ee13b7cf631fddf3829f01bd0b25c9ed13e14fd9
Czarne-oczki <czarnocki.michal2002@gmail.com>
2024-10-10 19:31:56 +0200

    add bms_hv header

---
+++ include/can_headers/PM09-CANBUS-BMSHVMAIN.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace PUTM_CAN {
+
+struct __attribute__((packed)) BmsHvMain {
+  uint16_t voltage_sum;
+  uint16_t current;
+  uint8_t temp_max;
+  uint8_t temp_avg;
+  uint16_t soc;
+};
+
+}  // namespace PUTM_CAN
