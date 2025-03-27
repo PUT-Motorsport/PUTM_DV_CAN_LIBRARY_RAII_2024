3381f73481f7c6ca368de8e4088fe3056e4ce6ef
Adam <Adam_Wasilewski_99@proton.me>
2025-03-27 23:32:23 +0100

    first commit oinbranch inv, modfied PC_MAIN_DATA

--- include/can_headers/PM09-CANBUS-PC.hpp
+++ include/can_headers/PM09-CANBUS-PC.hpp
@@ -5,15 +5,10 @@
 namespace PUTM_CAN {
 
 struct __attribute__((packed)) PcMainData {   
-    uint8_t rearRightInverterTemperature;
-    uint8_t rearLeftInverterTemperature;
-    uint8_t rearRightMotorTemperature;
-    uint8_t rearLeftMotorTemperature: 4;
+    
+    int16_t torque_current;
+    int16_t vechicle_speed;
     bool rtd :1;
-    uint8_t vehicleSpeed : 6;
-    uint8_t power : 6;
-    uint16_t rpm : 15;
-	bool invertersReady: 1;
     // bool rtd : 1;
     // uint8_t rearRightInverterTemperature;
     // uint8_t rearLeftInverterTemperature;
