c471297b7c4eb890285ccd42f05ec9759464ed5a
Adam <Adam_Wasilewski_99@proton.me>
2025-04-30 00:48:21 +0200

    added changed to canbus amktemp

--- include/can_headers/PM09-CANBUS-AMKTEMP.hpp
+++ include/can_headers/PM09-CANBUS-AMKTEMP.hpp
@@ -6,15 +6,15 @@ namespace PUTM_CAN {
 
 struct __attribute__((packed)) AmkTempData {  
 
-    int8_t motor_temp_fl;
-    int8_t motor_temp_fr;
-    int8_t motor_temp_rl;
-    int8_t motor_temp_rr;
+    uint8_t motor_temp_fl;
+    uint8_t motor_temp_fr;
+    uint8_t motor_temp_rl;
+    uint8_t motor_temp_rr;
 
-    int8_t inverter_temp_fl;
-    int8_t inverter_temp_fr;
-    int8_t inverter_temp_rl;
-    int8_t inverter_temp_rr;
+    uint8_t inverter_temp_fl;
+    uint8_t inverter_temp_fr;
+    uint8_t inverter_temp_rl;
+    uint8_t inverter_temp_rr;
   
 };
 
