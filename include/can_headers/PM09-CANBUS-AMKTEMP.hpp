1c56bfdb748683a4ec9d9686c8b3221d3778b1ed
Adam <Adam_Wasilewski_99@proton.me>
2025-05-10 20:01:18 +0200

    fixed positions of inverters temp, and addded frame based on github

--- include/can_headers/PM09-CANBUS-AMKTEMP.hpp
+++ include/can_headers/PM09-CANBUS-AMKTEMP.hpp
@@ -6,16 +6,18 @@ namespace PUTM_CAN {
 
 struct __attribute__((packed)) AmkTempData {  
 
-    uint8_t motor_temp_fl;
-    uint8_t motor_temp_fr;
-    uint8_t motor_temp_rl;
-    uint8_t motor_temp_rr;
-
-    uint8_t inverter_temp_fl;
-    uint8_t inverter_temp_fr;
-    uint8_t inverter_temp_rl;
     uint8_t inverter_temp_rr;
-  
+    uint8_t inverter_temp_rl;
+
+    uint8_t motor_temp_rr;
+    uint8_t motor_temp_rl;
+
+    uint8_t inverter_temp_fr;
+    uint8_t inverter_temp_fl;
+    
+    uint8_t motor_temp_fr;
+    uint8_t motor_temp_fl;
+
 };
 
 
--- include/can_headers/PM09-CANBUS-FRONTBOX.hpp
+++ include/can_headers/PM09-CANBUS-FRONTBOX.hpp
@@ -12,11 +12,7 @@ struct __attribute__((packed)) FrontboxDriverInput {
 };
 
 struct __attribute__((packed)) FrontboxData {
-  uint8_t s;
-  int16_t front_left_suspension;
-  int16_t front_right_suspension;
-  uint8_t front_left_hub_temperature;
-  uint8_t front_right_hub_temperature;
+  
   bool sense_left_kill : 1;
   bool sense_right_kill : 1;
   bool sense_driver_kill : 1;
@@ -25,6 +21,12 @@ struct __attribute__((packed)) FrontboxData {
   bool sense_overtravel : 1;
   bool sense_right_wheel : 1;
   bool is_braking : 1;
+  bool apps : 1;
+  uint8_t apps_implausibility;
+  int16_t front_left_suspension;
+  int16_t front_right_suspension;
+  
+ 
 };
 
 }  // namespace PUTM_CAN
