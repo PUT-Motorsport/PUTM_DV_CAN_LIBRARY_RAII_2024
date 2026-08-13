1036b92e6322e9f6ae3d2948026909fe6f5820a8
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2026-08-13 23:17:35 +0200

    refactor bms hv

--- include/can_headers/PM09-CANBUS-BMSHVMAIN.hpp
+++ include/can_headers/PM09-CANBUS-BMSHVMAIN.hpp
@@ -4,10 +4,15 @@ namespace PUTM_CAN {
 
 struct __attribute__((packed)) BmsHvMain {
   uint16_t voltage_sum;
-  int16_t current;
-  uint8_t temp_max;
-  uint8_t temp_avg;
-  uint16_t soc;
+  int16_t  current;
+  uint8_t  temp_max;
+  uint8_t  temp_avg;
+  
+  uint16_t soc : 10;
+  uint16_t ok : 1;
+  uint16_t precharge : 1;
+  uint16_t ts_on : 1;
+  uint16_t unused : 3;
 };
 
 }  // namespace PUTM_CAN
